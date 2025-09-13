2f478f7b2710a0e8281f88b1f3f85d5ce7d4f5fb
Harini007m <harinimgb@gmail.com>
2025-09-13 08:48:23 +0530

    Fixed bug in prblm 6 - added solution

--- problem6.py
+++ problem6.py
@@ -7,9 +7,15 @@ def is_balanced(s):
         if char in mapping.values():
             stack.append(char)
         elif char in mapping.keys():
-            if stack.pop() != mapping[char]:
+            if not stack or stack.pop() != mapping[char]:
                 return False
-    return True
+    return not stack
 
 print(is_balanced("()[]{}"))
 print(is_balanced("(]"))
+print(is_balanced("([)]"))  # nested different
+print(is_balanced("((()))"))  # nested same
+print(is_balanced("())"))  # extra closing
+print(is_balanced("(()"))  # extra opening
+print(is_balanced(""))  # empty
+print(is_balanced("a(b)c"))  # with non-brackets
